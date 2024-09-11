85e14beaff0fb9cf367a804436bb9f0180fdb000
Tigran <aruttigran1203@yandex.ru>
2024-09-11 19:26:30 +0300

    errors fix

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/jackc/pgx/v5"
@@ -11,13 +12,7 @@ import (
 	"github.com/mi4r/gophermart/internal/auth"
 )
 
-const (
-	InvalidRequest      string = "Invalid request"
-	HashedPasswordError string = "Server can't hash the password"
-	UserNotFoundError   string = "User not found"
-	LoginUsedError      string = "Login has already taken"
-	InvalidPassword     string = "Invalid password"
-)
+var errEmptyLoginOrPassword = errors.New("login or password cannot be empty")
 
 // Ping
 // @Summary Health check of the server
@@ -32,42 +27,48 @@ func (s *Server) pingHandler(c echo.Context) error {
 func (s *Server) registerHandler(c echo.Context) error {
 	var user storage.User
 	if err := c.Bind(&user); err != nil {
-		return c.JSON(http.StatusBadRequest, InvalidRequest)
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+	if user.Login == "" || user.Password == "" {
+		return c.String(http.StatusBadRequest, errEmptyLoginOrPassword.Error())
 	}
 
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, HashedPasswordError)
+		return c.String(http.StatusInternalServerError, err.Error())
 	}
 	user.Password = string(hashedPassword)
 
 	if err := s.storage.UserCreate(user); err != nil {
-		return c.JSON(http.StatusConflict, err.Error())
+		return c.String(http.StatusConflict, err.Error())
 	}
 
 	cookie := auth.GetUserCookie(user.Login)
 	c.SetCookie(cookie)
-	return c.JSON(http.StatusOK, "The user has been successfully registered and authenticated")
+	return c.String(http.StatusOK, "The user has been successfully registered and authenticated")
 }
 
 func (s *Server) loginHandler(c echo.Context) error {
 	var user storage.User
 	if err := c.Bind(&user); err != nil {
-		return c.JSON(http.StatusBadRequest, InvalidRequest)
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+	if user.Login == "" || user.Password == "" {
+		return c.String(http.StatusBadRequest, errEmptyLoginOrPassword.Error())
 	}
 
 	userStored, err := s.storage.UserReadOne(user.Login)
 	if err == pgx.ErrNoRows {
-		return c.JSON(http.StatusUnauthorized, UserNotFoundError)
+		return c.String(http.StatusUnauthorized, err.Error())
 	} else if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
 	if err = bcrypt.CompareHashAndPassword([]byte(userStored.Password), []byte(user.Password)); err != nil {
-		return c.JSON(http.StatusUnauthorized, InvalidPassword)
+		return c.String(http.StatusUnauthorized, err.Error())
 	}
 
 	cookie := auth.GetUserCookie(user.Login)
 	c.SetCookie(cookie)
-	return c.JSON(http.StatusOK, "The user has been successfully authenticated")
+	return c.String(http.StatusOK, "The user has been successfully authenticated")
 }
