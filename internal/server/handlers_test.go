a0b23ba4d85a07741bf37ce54d6fe6f7492e6de1
Tigran <aruttigran1203@yandex.ru>
2024-09-15 11:00:01 +0300

    add tests for login

--- internal/server/handlers_test.go
+++ internal/server/handlers_test.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/mi4r/gophermart/internal/storage"
 
@@ -42,7 +43,6 @@ func (m *MockStorage) Open() error {
 }
 
 func (m *MockStorage) Close() {
-	return
 }
 
 func TestUserRegisterHandler(t *testing.T) {
@@ -90,6 +90,67 @@ func TestUserRegisterHandler(t *testing.T) {
 			c := e.NewContext(req, rec)
 
 			if assert.NoError(t, server.userRegisterHandler(c)) {
+				assert.Equal(t, tt.expectedCode, rec.Code)
+				assert.Equal(t, tt.expectedBody, rec.Body.String())
+			}
+		})
+	}
+}
+
+func TestUserLoginHandler(t *testing.T) {
+	e := echo.New()
+	mockStorage := new(MockStorage)
+	server := &Server{storage: mockStorage}
+
+	tests := []struct {
+		name            string
+		requestBody     string
+		mockUser        storage.User
+		mockUserError   error
+		mockPasswordCmp bool
+		expectedCode    int
+		expectedBody    string
+	}{
+		{
+			name:            "Successful Login",
+			requestBody:     `{"login": "testuser", "password": "testpass"}`,
+			mockUser:        storage.User{Creds: storage.Creds{Login: "testuser", Password: "hashedpass"}},
+			mockUserError:   nil,
+			mockPasswordCmp: true,
+			expectedCode:    http.StatusOK,
+			expectedBody:    successUserLogin,
+		},
+		{
+			name:            "Invalid Login or Password",
+			requestBody:     `{"login": "testuser", "password": "wrongpass"}`,
+			mockUser:        storage.User{Creds: storage.Creds{Login: "testuser", Password: "hashedpass"}},
+			mockUserError:   nil,
+			mockPasswordCmp: false,
+			expectedCode:    http.StatusUnauthorized,
+			expectedBody:    errPasswordInvalid.Error(),
+		},
+		{
+			name:            "User Not Found",
+			requestBody:     `{"login": "nonexistent", "password": "testpass"}`,
+			mockUser:        storage.User{},
+			mockUserError:   pgx.ErrNoRows,
+			mockPasswordCmp: false,
+			expectedCode:    http.StatusUnauthorized,
+			expectedBody:    pgx.ErrNoRows.Error(),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockStorage.On("UserReadOne", mock.Anything).Return(tt.mockUser, tt.mockUserError)
+			mockStorage.On("PasswordCompare", mock.Anything).Return(tt.mockPasswordCmp)
+
+			req := httptest.NewRequest(http.MethodPost, "/api/user/login", strings.NewReader(tt.requestBody))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+
+			if assert.NoError(t, server.userLoginHandler(c)) {
 				assert.Equal(t, tt.expectedCode, rec.Code)
 				assert.Equal(t, tt.expectedBody, rec.Body.String())
 			}
